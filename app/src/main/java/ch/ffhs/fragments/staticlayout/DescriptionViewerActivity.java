ee7d77dbce6b42ac9f9dfde4dc38ab3c673c3e33
Shankar Ram <Shankar Ram>
2017-05-07 09:56:12 +0200

    initial commit

---
+++ app/src/main/java/ch/ffhs/fragments/staticlayout/DescriptionViewerActivity.java
@@ -0,0 +1,80 @@
+package ch.ffhs.fragments.staticlayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import ch.ffhs.fragments.staticlayout.TeamsFragment.ListSelectionListener;
+
+//Several Activity lifecycle methods are instrumented to emit LogCat output
+//so you can follow this class' lifecycle
+public class DescriptionViewerActivity extends Activity implements
+		ListSelectionListener {
+
+	public static String[] mTitleArray;
+	public static String[] mQuoteArray;
+	private DescriptionsFragment mDetailsFragment;
+
+	private static final String TAG = "DescriptionViewerActivity";
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Get the string arrays with the titles and qutoes
+		mTitleArray = getResources().getStringArray(R.array.Teams);
+		mQuoteArray = getResources().getStringArray(R.array.Descriptions);
+		
+		setContentView(R.layout.main);
+		
+		// Get a reference to the DescriptionsFragment
+		mDetailsFragment = (DescriptionsFragment) getFragmentManager()
+				.findFragmentById(R.id.details);
+	}
+
+	// Called when the user selects an item in the TeamsFragment
+	@Override
+	public void onListSelection(int index) {
+		if (mDetailsFragment.getShownIndex() != index) {
+
+			// Tell the QuoteFragment to show the quote string at position index
+			mDetailsFragment.showQuoteAtIndex(index);
+		}
+	}
+	
+	@Override
+	protected void onDestroy() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onDestroy()");
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onPause()");
+		super.onPause();
+	}
+
+	@Override
+	protected void onRestart() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onRestart()");
+		super.onRestart();
+	}
+
+	@Override
+	protected void onResume() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onResume()");
+		super.onResume();
+	}
+
+	@Override
+	protected void onStart() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onStart()");
+		super.onStart();
+	}
+
+	@Override
+	protected void onStop() {
+		Log.i(TAG, getClass().getSimpleName() + ":entered onStop()");
+		super.onStop();
+	}
+
+}
